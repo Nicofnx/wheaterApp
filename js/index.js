5a3d3792d69b9878b4bc7b723b949a4f14b1a9c2
Nicolas Rodriguez <nicolasarielrodriguez@gmail.com>
2022-08-28 19:09:49 -0300

    Agregado de sistema de favoritos

--- js/index.js
+++ js/index.js
@@ -49,39 +49,167 @@ const tomorrow = [{
     humedity: 55
 },];
 
-//Se realizara la busqueda desde el serch la cuidad que desea buscar. Cuando veamos storage se almacenaran las favoritas y cuando veamos geolocalizacion se realizara una busqueda por defecto en la ubicacion del usuario.
-let choseCity = prompt('Ingresa ciudad ("Lujan", "Pueblo nuevo" o "Rodriguez")');
+let menu = parseInt(prompt('Ingrese 1 para saber el tiempo\nIngrese 2 para anadir ciudades a favoritos'))
 
-//Itero los objetos para obtener los datos que necesito de la resp de la api.
-const dataClimaToday = (dataDay, tomorrow) => {
+if(menu == 1){
+    //Se realizara la busqueda desde el serch la cuidad que desea buscar. Cuando veamos storage se almacenaran las favoritas y cuando veamos geolocalizacion se realizara una busqueda por defecto en la ubicacion del usuario.
+    let choseCity = prompt('Ingresa ciudad ("Lujan", "Pueblo nuevo" o "Rodriguez")');
     
-
-    for (let index = 0; index < dataDay.length; index++){
+    //Itero los objetos para obtener los datos que necesito de la resp de la api.
+    const dataClimaToday = (dataDay, tomorrow) => {
         
-        const {city} = dataDay[index];
-        
-        if( choseCity == city){       
-            const {city, temp, tempMax, tempMin, humedity} = dataDay[index];
-            //A futuro en vez de un 'alert' se actualizara la card correspondiente al dia.                              
-            alert(`La temperatura de hoy en ${city} es de ${temp}°C con una humedad del ${humedity}% \nLa temperatura minima es de ${tempMin}°C y la temperatura maxima es de ${tempMax}°C`);
+    
+        for (let index = 0; index < toDay.length; index++){
+            
+            const {city} = toDay[index];
+            
+            if( choseCity == city){       
+                const {city, temp, tempMax, tempMin, humedity} = toDay[index];
+                //A futuro en vez de un 'alert' se actualizara la card correspondiente al dia.                              
+                alert(`La temperatura de hoy en ${city} es de ${temp}°C con una humedad del ${humedity}% \nLa temperatura minima es de ${tempMin}°C y la temperatura maxima es de ${tempMax}°C`);
+                }
+            
+            
+        }
+        for (let index = 0; index < tomorrow.length; index++) {
+    
+            const {city} = tomorrow[index];
+    
+            if( choseCity == city){                   
+                const {temp, tempMax, tempMin, humedity} = tomorrow[index];                
+                alert(`La temperatura de manana sera de ${temp}°C con una humedad del ${humedity}% \nCon una temperatura minima de ${tempMin}°C y temperatura maxima es de ${tempMax}°C`);
+                
             }
-        
+            
+        }
+    }
+    //Llamada a la funcion de la iteracion, donde por parametro le paso el objeto a iterar.
+    dataClimaToday(toDay, tomorrow)
+}
+
+//Segunda opcion del menu para el agregado de ciudades a favoritos
+else if (menu == 2) {
+
+    const favoriteCitys = [];
+
+    let newFav=''
+   
+    let listCitysFavs = ''
+    
+    //Bloque de funciones
+
+    //Fuencion para agregar a favoritos
+    const addFavorites= (favoriteCitys) => {
+        newFav = prompt('Ingrese ciudad a guardar en favoritos o "ESC" para finalizar')
+        if(newFav != 'ESC'){
+            favoriteCitys.push(newFav)
+            alert (`Ciudad ${newFav} agregada`)
+        }
         
     }
-    for (let index = 0; index < tomorrow.length; index++) {
-
-        const {city} = tomorrow[index];
-
-        if( choseCity == city){                   
-            const {temp, tempMax, tempMin, humedity} = tomorrow[index];                
-            alert(`La temperatura de manana sera de ${temp}°C con una humedad del ${humedity}% \nCon una temperatura minima de ${tempMin}°C y temperatura maxima es de ${tempMax}°C`);
+    //Funciona para ver lista de favoritos
+    const seeCitysFavs = (favoriteCitys) =>{
+        listCitysFavs='';
+        for (let i = 1; i <= favoriteCitys.length; i++) {       
+            listCitysFavs +=  `Ciudad Nro ${i} ${favoriteCitys[i-1]}\n`   
+        }
+        return(listCitysFavs)
+    }
+    //Fuencion para eliminar una ciudad de la lista de favoritos
+    const deleteCityFavs = (favoriteCitys) => {
+        let optionDelete = parseInt(prompt(`Ingrese el numero de ciudad a borrar de favoritos:\n${seeCitysFavs(favoriteCitys)}`))
+        if(optionDelete > 0){
+            favoriteCitys.splice(optionDelete-1,1)
+            if(seeCitysFavs(favoriteCitys)==''){
+                alert('No hay mas ciudades en favoritos')
+            }
+            else{
+                alert(seeCitysFavs(favoriteCitys))
+            }
             
         }
         
     }
+    ////////////////////////////////
+    //Bloque de operaciones
+    let operaciones = ''
+            
+        while(operaciones != 4){
+
+            operaciones = parseInt(prompt('Ingrese:\n1 para agregar ciudad a favorito\n2 para ver todas las ciudades de favoritos\n3 para eliminar una ciudad de favoritos\n4 Para salir'))
+            //Operacion 1 agregado de ciudades a favoritos
+            if(operaciones == 1){          
+    
+                
+                do{
+                    addFavorites(favoriteCitys)
+                    
+                }
+                while(newFav != 'ESC')
+            }
+            //Operacion 2 para visualizar todas las ciudades en favoritos
+            else if(operaciones == 2) {
+                if( favoriteCitys == ''){
+                    alert('No hay ciudades en favoritos')
+                    
+                }
+                else{
+                    alert(seeCitysFavs(favoriteCitys))
+                    
+                }
+                                            
+                    
+            }
+            //Operacion 3 para eliminar ciudades de favoritos
+            else if (operaciones == 3){
+                if( favoriteCitys == ''){
+                    alert('No hay ciudades en favoritos')
+                }
+                else{
+                    deleteCityFavs(favoriteCitys)
+
+                }
+                
+                
+            }
+                
+                
+
+        }
+            
+        
+    
+    
+
+} else {
+    alert('Programa finalizado')
 }
 
 
-//Llamada a la funcion de la iteracion, donde por parametro le paso el objeto a iterar.
-dataClimaToday(toDay, tomorrow)
+//Bloque de Fechas. Se utilizara para luego dar en cada card la fecha correspondiente al dia de "Hoy" y a los 5 dias posteriores
+const monthNames = ["Enero", "Febrero", "marzo", "Abril", "Mayo", "Junio",
+  "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
+
+getLongMonthName = function(date) {
+    return monthNames[date.getMonth()];
+}
+
+getShortMonthName = function(date) {
+    return monthNames[date.getMonth()].substring(0, 3);
+}
+
+
+let date = new Date()
+console.log(date)
+
+let day = date.getDate()
+let month = getShortMonthName(date)
+
+console.log(day)
+console.log(month)
+
+
+
+
 
