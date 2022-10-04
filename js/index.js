71ca0aa79f64cb10d8580c30479349b2e3ccad17
Nicolas Rodriguez <nicolasarielrodriguez@gmail.com>
2022-10-04 14:46:54 -0300

    Boton borrar

--- js/index.js
+++ js/index.js
@@ -328,7 +328,7 @@ const listCitys = (city) =>{
                 <button id="${city} type="button" data-city="${city}" data-bs-dismiss="modal" class="btn-city" value="${city}">${city}</button> 
             </div>
             <div class="btnsdelete" >            
-                <button id="delete_${city}" data-delete="${city}" type="button" class="btn btn-danger"><i data-delete="${city}" class="fa-solid fa-trash-can"></i></button>          
+                <button id="delete_${city}" data-delete="${city}" type="button" class="btn btn-danger">Borrar</button>          
             </div>
         
         </div>
